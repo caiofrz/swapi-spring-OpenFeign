8f5300170196cfaa1d2f7aa10cbc9e45d25d27e7
Caio Ferraz <caioferrazalmeida.27@gmail.com>
2024-01-16 12:04:51 -0300

    initial commit

---
+++ src/main/java/br/com/AmeDigital/backend_challenge_swapi/BackendChallengeSwapiApplication.java
@@ -0,0 +1,13 @@
+package br.com.AmeDigital.backend_challenge_swapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendChallengeSwapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendChallengeSwapiApplication.class, args);
+	}
+
+}
