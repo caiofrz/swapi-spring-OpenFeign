23158d119fa6eac2adfae49926c03bcc09d3c5e5
Caio Ferraz <caioferrazalmeida.27@gmail.com>
2024-01-28 22:45:33 -0300

    feat(client - SWAPI): add @Setter in swapi responses

--- src/main/java/br/com/AmeDigital/backend_challenge_swapi/domain/client/SwapiResponse.java
+++ src/main/java/br/com/AmeDigital/backend_challenge_swapi/domain/client/SwapiResponse.java
@@ -2,10 +2,12 @@ package br.com.AmeDigital.backend_challenge_swapi.domain.client;
 
 import com.fasterxml.jackson.annotation.JsonAutoDetect;
 import lombok.Getter;
+import lombok.Setter;
 
 import java.util.List;
 
 @Getter
+@Setter
 @JsonAutoDetect(fieldVisibility = JsonAutoDetect.Visibility.ANY)
 public class SwapiResponse {
 
